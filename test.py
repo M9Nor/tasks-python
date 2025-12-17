43a4b7cebf33c8494cfffe640e0c03fef20d19d4
Mohammed Nour <azer9nor@gmail.com>
2025-12-17 21:48:40 +0300

    adding one

---
+++ test.py
@@ -0,0 +1,11 @@
+items = []
+for i in range(5):
+    item = input("Enter item:")
+    items.append(item)
+
+target = input("Enter target to Searche: ")
+
+if target in items:
+    print("Found")
+else:
+    print("Not Found")
